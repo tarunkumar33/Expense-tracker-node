861052e2e65c0706cad875a228c2a0ae900b2cdc
Tarun Kumar <tarunkumarpk33@gmail.com>
2022-10-18 19:39:07 +0530

    encrpt Password

--- BackEnd/controllers/user.js
+++ BackEnd/controllers/user.js
@@ -1,4 +1,5 @@
 const User=require('../models/user');
+const bcrypt=require('bcrypt');
 
 function isStringInvalid(string){
     if(string==undefined || string.length===0){
@@ -13,14 +14,17 @@ exports.checkUserExist=async (req,res,next)=>{
             console.log('User Not Exists');
             res.status(404).json({success:false,message:'User Not Exists'});
         }
-        else if(result[0].password===req.body.password){
-            res.json({success:true,message:'User Successfully Loggedin'});
-        }
         else{
-            console.log('password Incorrect');
-            res.status(401).json({success:false,message:'Password Incorrect'});
-        }
-        
+            bcrypt.compare(req.body.password,result[0].password,(err,bcryptRes)=>{
+                if(bcryptRes){
+                    res.json({success:true,message:'User Successfully Loggedin'});
+                }
+                else{
+                    console.log('password Incorrect');
+                    res.status(401).json({success:false,message:'Password Incorrect'});
+                }
+            })
+        } 
     }
     catch(err){
         res.status(500).json(err);
@@ -33,8 +37,16 @@ exports.postUser=async (req,res,next)=>{
         if(isStringInvalid(name)||isStringInvalid(email)||isStringInvalid(password)){
             return res.status(400).json({err:"Bad parameters, something is missing"});
         }
-        const result=await User.create(req.body);
-        res.status(201).json(result);
+        const saltRounds=10;
+        bcrypt.hash(password,saltRounds,async (err,hash)=>{
+            try{
+                const result=await User.create({name,email,password:hash});
+                res.status(201).json({success:true,message:'Successfully Signed Up'});
+            }
+            catch(err){
+                res.status(500).json(err);
+            }
+        });
     }
     catch(err){
         res.status(500).json(err);
